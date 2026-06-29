805e03ec1a9065231402adbdc48a53331abbbce4
Trinetra Kumar <trinetrakumar7250@gmail.com>
2026-06-29 12:53:24 +0530

    Fix: Resolved SonarQube issue jssecurity:S5147 in Backend/controllers/teacher_controller/course_controller.js

--- Backend/controllers/teacher_controller/class_controller.js
+++ Backend/controllers/teacher_controller/class_controller.js
@@ -1,5 +1,6 @@
 const classModel = require("../../models/class_model");
 const courseModel = require("../../models/course_model");
+const mongoose = require('mongoose');
 
 module.exports.createClass = async function (req, res) {
   try {
@@ -15,7 +16,7 @@ module.exports.createClass = async function (req, res) {
       higher,
       course,
     });
-    const myCourse = await courseModel.findOne({ _id: course });
+    const myCourse = await courseModel.findOne({ _id: mongoose.Types.ObjectId(course) });
     myCourse.classes.push(myClass._id);
     await myCourse.save();
     res.status(201).send({ class_list: myCourse.classes, class: myClass });
@@ -31,8 +32,10 @@ module.exports.getClasses = async function (req, res) {
     if (!classes || classes.length === 0) {
       return res.status(404).send("No classes");
     }
-    // Using $in with an array of strings to prevent NoSQL injection
-    const myClasses = await classModel.find({ _id: { $in: classes.map((id) => id.toString()) } });
+    // Using $in with an array of mongoose.Types.ObjectId to prevent NoSQL injection
+    const myClasses = await classModel.find({
+      _id: { $in: classes.map((id) => mongoose.Types.ObjectId(id.toString())) },
+    });
     if (!myClasses || myClasses.length === 0) {
       return res.status(404).send("No classes");
     }
